b4bf0945959ea63b7828685a93336c0a4104a3d3
Dotan Di Castro <dotan.dicastro@gmail.com>
2022-12-15 06:31:05 +0200

    succeeded to install sb3 and mujoco py for half cheetah

---
+++ src/run_half_cheetah.py
@@ -0,0 +1,23 @@
+import gym
+import numpy as np
+
+from stable_baselines3 import SAC
+
+env = gym.make("HalfCheetah-v2")
+
+model = SAC("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=1000, log_interval=4)
+
+# model.save("sac_pendulum")
+
+# del model  # remove to demonstrate saving and loading
+
+# model = SAC.load("sac_pendulum")
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        obs = env.reset()
